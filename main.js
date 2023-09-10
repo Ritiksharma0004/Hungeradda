8e865b7714da0e6a7ef5fe72b939f312308829dc
Ritiksharma0004 <142799642+Ritiksharma0004@users.noreply.github.com>
2023-09-10 15:25:18 +0530

    Main page files

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function openForm () {
+    document.getElementById("myForm").style.display = "block";
+    // alert("I am working");
+}
+
+function minimizeForm () {
+    document.getElementById("myForm").style.display = "none";
+}
